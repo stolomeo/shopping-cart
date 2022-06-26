ce44267ba3d35aa23a712e5423af92460f0a657a
stolomeo <tolomeosebastian@gmail.com>
2022-06-26 15:20:35 -0400

    Structure project

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home";
+import Shop from "./pages/Shop";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
