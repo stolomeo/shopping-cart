135003102bf80bd37e3371bcb24c48b279f9626e
stolomeo <tolomeosebastian@gmail.com>
2022-07-13 19:35:14 -0400

    Created tests for Products

---
+++ src/components/Products/Products.test.tsx
@@ -0,0 +1,19 @@
+import { render } from "@testing-library/react";
+import { axe, toHaveNoViolations } from "jest-axe";
+import Products from "./Products";
+
+describe("<Products />", () => {
+  const renderComponent = () => render(<Products />);
+  expect.extend(toHaveNoViolations);
+
+  it("should match snapshot", () => {
+    const { container } = renderComponent();
+    expect(container).toMatchSnapshot();
+  });
+
+  it("should demonstrate this matcher's usage with react testing library", async () => {
+    const { container } = renderComponent();
+    const results = await axe(container);
+    expect(results).toHaveNoViolations();
+  });
+});
